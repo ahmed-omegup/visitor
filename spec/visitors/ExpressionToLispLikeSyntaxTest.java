6782615b736dc8433d1d3dcfbcfa4b239d1784f8
ahmed <ahmed>
2026-04-08 11:30:08 +0100

    improve coverage

--- spec/visitors/ExpressionToLispLikeSyntaxTest.java
+++ spec/visitors/ExpressionToLispLikeSyntaxTest.java
@@ -1,5 +1,6 @@
 package spec.visitors;
 
+import static java.util.List.of;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import org.junit.jupiter.api.Test;
@@ -26,6 +27,27 @@ abstract class ExpressionToLispLikeSyntaxTestBase<E> extends TestBase<E> {
                 factory.disjunction(factory.logicalNot(factory.variableReference("ready")), factory.exponentiation(factory.literal("2"), factory.literal("3")))
             ))
         );
+    }
+
+    @Test
+    void lispLikeSyntaxPrintsEveryOperatorForm() {
+        assertEquals("(- 3 1)", renderLispLike(factory.subtraction(factory.literal("3"), factory.literal("1"))));
+        assertEquals("(/ 6 2)", renderLispLike(factory.division(factory.literal("6"), factory.literal("2"))));
+        assertEquals("(neg 3)", renderLispLike(factory.negation(factory.literal("3"))));
+        assertEquals("(mod 7 3)", renderLispLike(factory.modulo(factory.literal("7"), factory.literal("3"))));
+        assertEquals("(= 1 1)", renderLispLike(factory.equality(factory.literal("1"), factory.literal("1"))));
+        assertEquals("(!= 1 2)", renderLispLike(factory.inequality(factory.literal("1"), factory.literal("2"))));
+        assertEquals("(< 1 2)", renderLispLike(factory.lessThan(factory.literal("1"), factory.literal("2"))));
+        assertEquals("(> 2 1)", renderLispLike(factory.greaterThan(factory.literal("2"), factory.literal("1"))));
+        assertEquals("(and 1 1)", renderLispLike(factory.conjunction(factory.literal("1"), factory.literal("1"))));
+    }
+
+    @Test
+    void lispLikeSyntaxPrintsLeavesAndCallShapes() {
+        assertEquals("7", renderLispLike(factory.literal("7")));
+        assertEquals("name", renderLispLike(factory.variableReference("name")));
+        assertEquals("(sum 1 2)", renderLispLike(factory.functionCall(factory.variableReference("sum"), of(factory.literal("1"), factory.literal("2")))));
+        assertEquals("(noop)", renderLispLike(factory.functionCall(factory.variableReference("noop"), of())));
     }
 }
 
