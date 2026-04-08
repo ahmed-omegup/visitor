319c092cf057b232fde1b5fadb88d3e0c0f77652
ahmed <ahmed>
2026-04-08 18:29:17 +0100

    version 2 - spec ready

---
+++ port/IHandlerFactory2.java
@@ -0,0 +1,18 @@
+package port;
+
+import java.util.List;
+import java.util.Map;
+import java.util.function.BiFunction;
+import java.util.function.Function;
+import java.util.function.Supplier;
+
+import lib.expression.ExpressionV1;
+
+public interface IHandlerFactory2<E> extends IHandlerFactory<E> {
+
+    IExpressionFactory2<E> expressionFactory();
+
+    Function<E, IExpressionDict2<Integer>> histogram2();
+
+    Function<E, String> i18nDict(String lang);
+}
